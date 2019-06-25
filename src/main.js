03cb44ae2994dd208af4e2666fb6ddc613804902
Gabe Dunn <gabe.dunn@shaw.ca>
2019-06-25 11:26:09 -0600

    add unhandled/uncaught exception/rejection handling to the main function

--- src/main.js
+++ src/main.js
@@ -6,6 +6,17 @@
 import { devmod } from './devmod'
 import { sendRolesMessage } from './processes/sendRolesMessage'
 
+// If an unhandled rejection occurs, log it and exit the program.
+process.on('unhandledRejection', (err, p) => {
+  console.error('Unhandled Rejection:', err.stack, p)
+  process.exit(1)
+})
+// Same thing but for uncaught exception.
+process.on('uncaughtException', (err, p) => {
+  console.error('Uncaught Exception:', err.stack, p)
+  process.exit(1)
+})
+
 // If '--roles' is provided as an argument from the command line, send the roles message(s). Otherwise run the bot.
 if (process.argv.indexOf('--roles') !== -1) {
   sendRolesMessage()
