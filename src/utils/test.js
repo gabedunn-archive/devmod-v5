203f40ab22ff6eb848c90426c675be8f0f147590
Gabe Dunn <gabe.dunn@shaw.ca>
2019-06-26 00:23:05 -0600

    add test file

---
+++ src/utils/test.js
@@ -0,0 +1,27 @@
+/*
+ * Gabe Dunn 2019
+ * File to run to test various js methods and etc.
+ */
+
+// Main function to utilize await for db functions.
+const test = async () => {
+  const array = [
+    {
+      array: [1, 2, 3, 4]
+    },
+    {
+      array: [5, 6, 7, 8]
+    }
+  ]
+  const newArray = array.reduce((previousValue, currentValue) => {
+    const previousArray = previousValue
+    for (const item of currentValue.array) {
+      previousArray.push(item)
+    }
+    return previousArray
+  }, [])
+
+  console.log(newArray)
+}
+
+test()
