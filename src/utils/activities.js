df91caf4f626095975fe15d5036abdd18d409185
Gabe Dunn <gabe.dunn@shaw.ca>
2019-07-17 16:25:22 -0600

    add some new activities

--- src/utils/activities.js
+++ src/utils/activities.js
@@ -4,21 +4,24 @@
  */
 
 export const activities = [
-  'Counting your Good Boye Points.',
-  'Trying to unban SWAT SEAL.',
-  'Spamming the DMs of a random user.',
-  'Compiling.',
+  'Counting your good boye points (rip)...',
+  'Trying to unban SWAT SEAL...',
+  'Spamming the DMs of a random user...',
+  'Compiling...',
   'Having a snack.',
   'uncaughtException',
-  'Shitposting in #off-topic-chat.',
+  'Shitposting in #offtopic.',
   'Admiring egg.',
-  'Trying to exit vim.',
+  'Trying to exit vim...',
   'BEING A HUMAN.',
   '10011001101111100000011011110',
-  'Hacking the FBI.',
+  'Hacking the FBI...',
   'Serving NaN users.',
-  'on a Christian Server',
+  'on a Christian Server.',
   'et ur brokli',
   'this chat gets weird fasy',
-  'https://i.imgur.com/BVRHZzg.png'
+  'https://i.imgur.com/BVRHZzg.png',
+  'Complaining about the logo.',
+  'REEEEEEEEEEEEEEEEEEEEEE',
+  'Promoting VUE!'
 ]
